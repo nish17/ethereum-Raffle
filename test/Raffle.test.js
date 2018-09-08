e0c8769bf79e530e3a3917e2af67eb2a728ba9e1
Nimesh Solanki <nimesh3536@gmail.com>
2018-09-08 13:20:50 +0530

    add new tests

--- test/Raffle.test.js
+++ test/Raffle.test.js
@@ -18,4 +18,40 @@ describe("Lottery Contract", () => {
   it("deploys a contract", () => {
     assert.ok(raffle.options.address);
   });
+
+  it("adds one address to the players array", async () => {
+    await raffle.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    const players = await raffle.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+
+  it("adds multiple address to the players array", async () => {
+    await raffle.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    await raffle.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    await raffle.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei("0.02", "ether")
+    });
+    const players = await raffle.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
+  });
 });
